e4905269ad4b270d5957722f546cc9757d2c1973
simeon4real <simeon.udoh45@gmail.com>
2020-07-26 22:29:02 +0100

    added MyName component with props :zap:

---
+++ src/components/MyName.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello, My name is <strong>{{ name }}</strong>. </h1>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['name']
+  }
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 300; 
+}
+</style>
